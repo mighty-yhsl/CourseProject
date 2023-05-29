628378d12bb451a825f7402e811a6e237c96aa0e
laykmin15 <laykmin15@gmail.com>
2023-05-29 10:38:20 +0300

    First Controller

---
+++ CourseProject/Controllers/TransportController.cs
@@ -0,0 +1,17 @@
+﻿using CourseProject.BLL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject.Controllers
+{
+    public class TransportController:Controller
+    {
+        public TransportController()
+        { 
+        }
+
+        public ActionResult GetAllTransports([FromServices] TransportRepository transportRepository) 
+        {
+            return View(transportRepository.Get());
+        }
+    }
+}
---
+++ CourseProject/ViewModels/TransportVM.cs
@@ -0,0 +1,17 @@
+﻿namespace CourseProject.ViewModels
+{
+    public class TransportVM
+    {
+        public string Name { get; set; } = null!;
+
+        public int Speed { get; set; }
+
+        public int Weightt { get; set; }
+
+        public int EnginePower { get; set; }
+
+        public int Amount { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
